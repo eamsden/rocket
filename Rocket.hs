c1950ccbf95ac39159c862112f55113dbcd45601
Edward Amsden <edwardamsden@gmail.com>
2014-09-26 19:44:31 -0400

    rocket stuff

--- Rocket.hs
+++ Rocket.hs
@@ -4,5 +4,5 @@ main :: IO ()
 main = writeSTL 0.5 "rocket.stl" $ shell 3 $
   union
     [ cylinder2 32 2 50
-    , translate (-30) $ cylinder 30 30
+    , translate (0,0,-30) $ cylinder 30 30
     ]
